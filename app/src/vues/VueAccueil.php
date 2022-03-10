01058d5d3106e731ab18e8e41eee8de4a16f6727
TheoFaedo <73793294+TheoFaedo@users.noreply.github.com>
2022-03-10 12:25:53 +0100

    a

--- app/src/vues/VueAccueil.php
+++ app/src/vues/VueAccueil.php
@@ -21,7 +21,7 @@ class VueAccueil{
         <!DOCTYPE html>
         <html lang="fr">
         <head>
-            <meta charset="UTF-8">
+            <meta charset="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>CustomBox</title>
             <link rel="stylesheet" href="$BaseUrl/css/reset.css">
--- app/src/vues/VueListeProduits.php
+++ app/src/vues/VueListeProduits.php
@@ -41,7 +41,7 @@ class VueListeProduits{
         <!DOCTYPE html>
         <html lang="fr">
         <head>
-            <meta charset="UTF-8">
+            <meta charset="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>CustomBox</title>
             <link rel="stylesheet" href="$BaseUrl/css/reset.css">
